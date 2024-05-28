a96bda20fcc916903eb14e88a746c278c269138d
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2024-05-27 22:40:07 -0300

    :sparkles: :feat: iniciando servidor

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mfcbentes/go-gin-gorm/helper"
+	"github.com/rs/zerolog/log"
+)
+
 func main() {
+
+	log.Info().Msg("Starting server...")
+	routes := gin.Default()
+
+	routes.GET("", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "Welcome home")
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: routes,
+	}
+	err := server.ListenAndServe()
+	helper.ErrorPanic(err)
 
 }
